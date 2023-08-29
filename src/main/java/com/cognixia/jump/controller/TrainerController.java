07f3ae6936deaca0a0baaa8bae8e8a3901a5b400
Austin Rich <auserich@gmail.com>
2023-08-29 12:52:25 -0700

    Changed Type value to be two types: main and secondary. Added SQL script for dummy data, too

--- src/main/java/com/cognixia/jump/controller/TrainerController.java
+++ src/main/java/com/cognixia/jump/controller/TrainerController.java
@@ -6,13 +6,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.Trainer;
 import com.cognixia.jump.repository.TrainerRepository;
 
@@ -27,12 +30,12 @@ public class TrainerController {
 	PasswordEncoder encoder;
 	
 	@GetMapping("/trainer")
-	public List<Trainer> getUsers() {
+	public List<Trainer> getTrainers() {
 		return repo.findAll();
 	}
 	
-	@GetMapping("/user/{id}")
-	public ResponseEntity<?> getUserById(@PathVariable int id) {
+	@GetMapping("/trainer/{id}")
+	public ResponseEntity<?> getTrainerById(@PathVariable int id) {
 		
 		Optional<Trainer> trainer = repo.findById(id);
 		
@@ -44,8 +47,8 @@ public class TrainerController {
 		}
 	}
 
-	@PostMapping("/user")
-	public ResponseEntity<?> createUser( @RequestBody Trainer trainer ) {
+	@PostMapping("/trainer")
+	public ResponseEntity<?> createTrainer( @RequestBody Trainer trainer ) {
 		
 		trainer.setId(null);
 		
@@ -56,5 +59,28 @@ public class TrainerController {
 		Trainer created = repo.save(trainer);
 		
 		return ResponseEntity.status(201).body(created);
+	}
+	
+	@PutMapping("/trainer")
+	public ResponseEntity<?> updateTrainer(@RequestBody Trainer trainer) throws ResourceNotFoundException {
+		if (repo.existsById(trainer.getId())) {
+			Trainer updated = repo.save(trainer);
+			return ResponseEntity.status(200).body(updated);
+		}
+		
+		throw new ResourceNotFoundException("Trainer with id = " + trainer.getId() + " was not found");
+	}
+	
+	@DeleteMapping("/trainer/{id}")
+	public ResponseEntity<Trainer> deleteTrainer(@PathVariable int id) throws ResourceNotFoundException {
+		Optional<Trainer> found = repo.findById(id);
+		
+		if (found.isEmpty()) {
+			throw new ResourceNotFoundException("Product with id = " + id + " was not found");
+		}
+		
+		repo.deleteById(id);
+		
+		return ResponseEntity.status(200).body(found.get());
 	}
 }
