f09fa39a7573db491957538e25a0dcb8f0452de5
Austin Rich <auserich@gmail.com>
2023-08-29 21:07:14 -0700

    Fixed an issue preventin project to run

--- src/main/java/com/cognixia/jump/CapstoneProjectApplication.java
+++ src/main/java/com/cognixia/jump/CapstoneProjectApplication.java
@@ -3,7 +3,21 @@ package com.cognixia.jump;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(title="Capstone", version="1.0",
+			description="description",
+			contact=@Contact(name="contact info", email="contact email",url="contact url"),
+			license=@License(name="license name", url="license url"),
+			termsOfService="tos"),
+	externalDocs = @ExternalDocumentation(description="external description", url="external url")
+)
 public class CapstoneProjectApplication {
 
 	public static void main(String[] args) {
